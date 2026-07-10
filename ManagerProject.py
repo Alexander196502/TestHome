d51f360396b53c975d3ddfb791ab205e9b0bd788
Alexander196502 <vashugin@GMail.com>
2026-07-10 17:55:33 +0300

    Add files via upload

--- ManagerProject.py
+++ ManagerProject.py
@@ -20,6 +20,7 @@ def add_note():
         # Добавляем текст в конец поля
 
         txt.insert(tk.END, note + "\n")
+
     except Exception as e:
         messagebox.showerror("Ошибка", f"Не удалось прочитать файл:\n{e}")
 
@@ -67,6 +68,30 @@ win.geometry(f'{X}x{Y}+{pos_x}+{pos_y}')
 
 win.title('Менеджер заметок')
 #root.config(bg='gray')
+"""
+# --- МЕНЮ ---
+menubar = tk.Menu(win)
+win.config(menu=menubar)
+
+# Меню "Файл"
+file_menu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Добавить заметку из файла", command=add_note)
+file_menu.add_command(label="Очистить поле", command=del_note)
+file_menu.add_separator()
+file_menu.add_command(label="Сохранить заметку", command=write_note)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=win.quit)
+"""
+mainmenu = tk.Menu(win)
+win.config(menu=mainmenu)
+fm = tk.Menu(mainmenu, tearoff= 1)
+fm.add_command(label = "Открыть", command= add_note)
+fm.add_command(label = "Очистить", command= del_note)
+fm.add_command(label = "Сохранить", command= write_note)
+fm.add_separator()
+fm.add_command(label = "Закрыть", command= win.destroy)
+mainmenu.add_cascade(label ="File", menu=fm)
 
 # Создаём виджет Text
 txt = tk.Text(win, width=40, height=8)
@@ -77,8 +102,8 @@ txt.pack(side='left', padx=10, pady=10)
 scr = tk.Scrollbar(win, command=txt.yview)
 scr.pack(side='left', fill='y')
 txt.configure(yscrollcommand=scr.set)
-
-# Кнопка «Добавить заметку»
+"""
+### Кнопка «Добавить заметку»
 start_btn = tk.Button(win, text='Добавить заметку', font=('Arial', 9), command=add_note)
 start_btn.pack(side=tk.LEFT)
 
@@ -88,5 +113,5 @@ stop_btn.pack(side=tk.LEFT)
 
 write_btn = tk.Button(win, text='Сохранить заметку', font=('Arial', 9), command=write_note)
 write_btn.pack(side=tk.LEFT)
-
+"""
 win.mainloop()
