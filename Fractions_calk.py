80f62883e90d01068940b140f4d102d8bdfda498
Alexander Vashugin <vashugin@gmail.com>
2026-07-23 16:03:25 +0300

    23 jul Добавлены новые файлы и обновлены существующие

--- Fractions_calk.py
+++ Fractions_calk.py
@@ -2,6 +2,52 @@ from tkinter import *
 import math
 
 
+def validator(event):
+    # print(event.char)
+    entry_text = event.widget.get().strip()
+    correct_text = ''.join([t for t in entry_text if t in '0123456789'])
+    # print(entry_text, correct_text)
+    if entry_text != correct_text:
+        event.widget.delete(0, 'end')
+        event.widget.insert(0, correct_text)
+    if int(entry_text) == 0:
+        event.widget.delete(0, 'end')
+
+
+def validator_oper(event):
+    # print(event.char)
+    entry_text = oper.get().strip()
+    correct_text = ''.join([t for t in entry_text if t in '+-*/'])
+    # print(entry_text, correct_text)
+    if entry_text != correct_text:
+        event.widget.delete(0, 'end')
+        event.widget.insert(0, correct_text)
+
+
+def add(n1, d1, n2, d2):
+    n = n1 * d2 + n2 * d1
+    d = d1 * d2
+    return n, d
+
+
+def sub(n1, d1, n2, d2):
+    n = n1 * d2 - n2 * d1
+    d = d1 * d2
+    return n, d
+
+
+def mult(n1, d1, n2, d2):
+    n = n1 * n2
+    d = d1 * d2
+    return n, d
+
+
+def div(n1, d1, n2, d2):
+    n = n1 * d2
+    d = d1 * n2
+    return n, d
+
+
 def get_items():
     try:
         n1 = int(num1.get())
@@ -11,19 +57,17 @@ def get_items():
         znak = oper.get().strip()
     except Exception:
         znak = ''
+
     match znak:
         case '+':
-            n = n1 * d2 + n2 * d1
-            d = d1 * d2
+            n, d = add(n1, d1, n2, d2)
         case '-':
-            n = n1 * d2 - n2 * d1
-            d = d1 * d2
+            n, d = sub(n1, d1, n2, d2)
         case '*':
-            n = n1 * n2
-            d = d1 * d2
+            n, d = mult(n1, d1, n2, d2)
         case '/':
-            n = n1 * d2
-            d = d1 * n2
+            n, d = div(n1, d1, n2, d2)
+
     nod = math.gcd(n, d)
     n = n // nod
     d = d // nod
@@ -35,8 +79,12 @@ def get_items():
             d = ''
     elif n == d:
         int_p = 1
-        n = ""
-        d = ""
+        n = ''
+        d = ''
+    elif n == 0 and int_p == '':
+        int_p = 0
+        n = ''
+        d = ''
 
     int_part['text'] = str(int_p)
     num3['text'] = str(n)
@@ -45,39 +93,46 @@ def get_items():
 
 root = Tk()
 root.title('Fractions calculator')
-root.geometry('300x200+800+50')
+root.geometry('300x150+800+200')
+
 frame = Frame(root)
-frame.pack(pady=10)    # в root можно использовать только pack
+frame.pack(pady=10)  # в root теперь можно использовать только pack
 num1 = Entry(frame, width=2, font=('Arial', 15), justify='center')
-num1.grid(row=0, column=0)  # в frame можно использовать только grid
-line1 = Label(frame, text=chr(9135)*5)
+num1.grid(row=0, column=0)  # в frame теперь можно использовать только grid
+line1 = Label(frame, text=chr(9135) * 5)
 line1.grid(row=1, column=0)
 den1 = Entry(frame, width=2, font=('Arial', 15), justify='center')
 den1.grid(row=2, column=0)
 
 oper = Entry(frame, width=2, font=('Arial', 15), justify='center')
-oper.grid(row=1, column=1,padx=5)
+oper.grid(row=1, column=1, padx=5)
 
 num2 = Entry(frame, width=2, font=('Arial', 15), justify='center')
-num2.grid(row=0, column=2)  # в frame можно использовать только grid
-line2 = Label(frame, text=chr(9135)*5)
+num2.grid(row=0, column=2)
+line2 = Label(frame, text=chr(9135) * 5)
 line2.grid(row=1, column=2)
 den2 = Entry(frame, width=2, font=('Arial', 15), justify='center')
 den2.grid(row=2, column=2)
 
 equal = Button(frame, text='=', width=2, command=get_items)
-equal.config(font=('Arial', 12))
+equal.config(font='Arial 15')
 equal.grid(row=1, column=3, padx=5)
 
 int_part = Label(frame, text='  ')
-int_part.config(font='Arial 20', justify='center', bg='lightgrey')
+int_part.config(font='Arial 20', justify='center', bg='lightgray', width=2)
 int_part.grid(row=1, column=4)
 
-num3 = Label(frame, width=2, font=('Arial', 15), justify='center', bg='lightgrey')
-num3.grid(row=0, column=5)  # в frame можно использовать только grid
-line3 = Label(frame, text=chr(9135)*5)
+num3 = Label(frame, width=2, font=('Arial', 15), justify='center', bg='lightgray')
+num3.grid(row=0, column=5)
+line3 = Label(frame, text=chr(9135) * 5)
 line3.grid(row=1, column=5)
-den3 = Label(frame, width=2, font=('Arial', 15), justify='center', bg='lightgrey')
+den3 = Label(frame, width=2, font=('Arial', 15), justify='center', bg='lightgray')
 den3.grid(row=2, column=5)
+
+num1.bind('<KeyRelease>', validator)
+num2.bind('<KeyRelease>', validator)
+den1.bind('<KeyRelease>', validator)
+den2.bind('<KeyRelease>', validator)
+oper.bind('<KeyRelease>', validator_oper)
 
 root.mainloop()
--- dog_tkinter.py
+++ dog_tkinter.py
@@ -3,52 +3,63 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
-from io import BytesIO
+from  io import BytesIO
+
 
 
 def get_random_dog_image():
     try:
-        responce = requests.get('https://dog.ceo/api/breeds/image/random')
-        responce.raise_for_status()
-        data - responce.json()
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
         return data['message']
-    except Exeption as err:
-        messagebox.showerror(f'Ошибка запроса API')
+    except Exception as err:
+        messagebox.showerror('Ошибка', f'Ошибка запроса API {err}')
         return None
+
 
 def show_image():
     image_url = get_random_dog_image()
     if image_url:
         try:
-            responce = request.get(image_url, stream-True)
-            responce.raise_for_status()
-            img_data = BytesIO(responce.content)
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
             img = Image.open(img_data)
-            img.thumbnail((300, 300))
+            size = (int(width_spinbox.get()), int(height_spinbox.get()))
+            img.thumbnail(size)
             img = ImageTk.PhotoImage(img)
-            label.config(Image = Img)
+
+            new_window = Toplevel()
+            new_window.title('Новое окно')
+            label = ttk.Label(new_window, image=img)
+            label.config(image = img)
             label.image = img
+            label.pack(padx=10, pady=10)
+
         except requests.RequestException as err:
-            messagebox.showerror('Ошибка',f'Не удалось все получить')
-    progress,stop()
+            messagebox.showerror('Ошибка',f'Не удалось загрузить изображение {err}')
+    progress.stop()
 
 def prog():
     progress['value'] = 0
-    progress.start(30)
+    progress.start(20)
     root.after(3000, show_image)
 
 
-def spinbox():
-    width = int(width_spinbox.get())
-    height = int(height_spinbox.get())
-
+# def spinbox():
+#     width = int(width_spinbox.get())
+#     height = int(height_spinbox.get())
+#     coord = width, height
+#     return coord
 
 root = Tk()
+root.title("Собачки")
 root.geometry('360x420')
-root.title('Dogs')
 label = ttk.Label()
-label.pack(padx=10,pady=10)
-button = ttk.Button(text='Загрузить изображенгте', command=prog)
+label.pack(padx=10, pady=10)
+
+button = ttk.Button(text='Загрузить изображение', command = prog)
 button.pack(padx=10, pady=10)
 
 progress = ttk.Progressbar(mode='determinate', length=300)
@@ -56,12 +67,13 @@ progress.pack(padx=10, pady=10)
 
 width_label = ttk.Label(text='Ширина')
 width_label.pack(side='left', padx=(10,0))
-width_spinbox = ttk.Spinbox(from_=200, to=600, increment-50, width=5)
-width_spinbox.pack(side='left', padx=(10,0))
+width_spinbox = ttk.Spinbox(from_= 200, to=500, increment=50, width=5)
+width_spinbox.pack(side='left', padx=(0,10))
 
 height_label = ttk.Label(text='Высота')
 height_label.pack(side='left', padx=(10,0))
-height_spinbox = ttk.Spinbox(from_=200, to=600, increment-50, width=5)
-height_spinbox.pack(side='left', padx=(10,0))
+height_spinbox = ttk.Spinbox(from_= 200, to=500, increment=50, width=5)
+height_spinbox.pack(side='left', padx=(0,10))
+
 
 root.mainloop()
